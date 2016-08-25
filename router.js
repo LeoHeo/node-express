008e7f0e34e54372849939cc075bae1c16ddcc58
LeoHeo <hjh5488@gmail.com>
2016-08-25 10:07:44 +0900

    init watcha: watcha news render

--- router.js
+++ router.js
@@ -1,13 +1,24 @@
 var express  = require("express");
 var router = express.Router();
 
+var httpRequest = require("request");
+
 // Router
 router.get("/", function(req, res) {
     return res.render("home", {animals: ["dog", "cat", "python"]});
 });
 
-router.get("/about", function(req, res) {
+router.get("/about/", function(req, res) {
     return res.render("about");
+});
+
+router.get("/watcha/", function(req, res) {
+    var url = "https://watcha.net/home/news.json?page=1&per=20";
+
+    httpRequest(url, function(error, httpResponse, body) {
+        var data = JSON.parse(body);
+        return res.render("watcha", {newsItems: data["news"]});
+    });
 });
 
 module.exports = router;
